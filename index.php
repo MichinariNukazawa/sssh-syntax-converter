ec9e4520913f9ea00c14c7b0760defc51172c7ef
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2014-02-06 22:10:40 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>highlight builder</title>
+	<!--
+	<link rel="shortcut icon" href="icon.ico">
+	-->
+	<script type="text/javascript" src="scripts.js"></script>
+	<LINK href="style.css" rel="stylesheet" type="text/css">
+
+
+
+</head>
+<body>
+	<h1>SSSH<font size="-1"> -Standalone Static Syntax Highlight builder-</font></h1>
+
+	<h2>使い方</h2>
+	<div class="text">
+	・「ソースコード」にコードを貼る<br />
+	・「プレビュー」「HTML」に結果が表示される<br />
+	・「HTML」をコピーして、コードを表示したいページに貼り付ける<br />
+	<br />
+	ソースコードを表示可能なHTMLに変換します。
+	簡素なシンタックスハイライトを付けることができます。<br />
+	ライブラリをWebページに読み込む必要はありません。<br />
+	</div>
+	<!--
+	詳しい使い方などは開発者の<a href=>ブログ記事</a>をご覧ください<br />
+	-->
+		<h3>設定</h3>
+		行番号:
+		<input type="checkbox" name="is_line_number" checked 
+		 id="is_line_number" onclick="cb_line_number_checkbox()">
+		<select name="kind_line_number"
+		 id="kind_line_number" onclick="cb_kind_line_number()">
+			<option value="plain">プレーンテキスト</option>
+			<!--
+			<option value="js">動的(JavaScript使用) 未実装</option>
+			<option value="js_css">動的(JS,CSS使用) 未実装</option>
+			-->
+		</select>
+		<br />
+		<font size="-2">※プレーンテキスト行番号を使うと、コードがコピーペーストしずらくなってしまいます</font>
+		<br />
+		シンタックスハイライト:
+		<select name="kind_syntax" id="kind_syntax" onclick="cb_kind_syntax()">
+			<option value="html">HTML系(HTML,PHP,JS,CSS)</option>
+			<option value="c">C言語系(未実装)</option>
+			<option value="none">使用しない</option>
+		</select>
+		<br />
+		<!--
+		TABの扱い:
+		<select name="kind_tab" id="kind_tab" onclick="cb_kind_tab()">
+			<option value="span">フルサイズ(8タブ表示 spanタグ)</option>
+			<option value="escape">エスケープ(2~4タブ表示 &#x 0009;)</option>
+		</select>
+		-->
+		<h3> ソースコード：<font size="-1">(ここに貼り付けます)</font></h3>
+		<textarea rows="5" style="width:90%" 
+		 id='code'
+		 onkeyup="cb_onkeyup_code()">
+		 <div>hello html</div>
+		 <span style="color:blue">blue text</span>
+#include <stdio.h>
+int main(){
+return 0;
+}
+
+<div>
+class1
+</div>
+</textarea>
+		<br />
+		<h3>プレビュー:</h3>
+		<div id="preview" style="border : 3px solid #eeeeff; width:90%" ></div>
+		<h3>HTML:</h3>
+		<textarea style="width:90%" 
+		 cols="50" rows="12" id="html_output" readonly>
+		</textarea>
+		<br />
+		
+</body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,162 @@
+	window.onload = function(){
+		console.log("load window");
+		conv();
+	}
+	function cb_onkeyup_code(){
+		console.log("cb_onkeyup_code");
+		conv();
+	}
+	function cb_line_number_checkbox(){
+		console.log("cb_line_number_checkbox");
+		var obj_is_lnum = document.getElementById('is_line_number');
+		var obj_kind_lnum = document.getElementById('kind_line_number');
+		if (obj_is_lnum.checked){
+			obj_kind_lnum.disabled = false;
+		}
+		else {
+			obj_kind_lnum.disabled = true;
+		}
+		conv();
+	}
+	function cb_kind_line_number(){
+		console.log("cb_kind_line_number");
+		conv();
+	}
+	function cb_kind_syntax(){
+		console.log("cb_kind_syntax");
+		conv();
+	}
+	function cb_kind_tab(){
+		console.log("cb_kind_tab");
+		conv();
+	}
+
+	function conv(){
+		console.log("conv");
+		var obj_is_lnum = document.getElementById('is_line_number');
+
+		var obj_code = document.getElementById('code');
+		var obj_preview = document.getElementById('preview');
+		var obj_output = document.getElementById('html_output');
+		var obj_syntax = document.getElementById('kind_syntax');
+		var code = obj_code.value;
+		//console.log(code);
+
+		// HTMLで表示できない記号を特殊文字に変換する
+		code = htmlspecialchars(code);
+		// シンタックスハイライトを行う
+		var kind_syntax = obj_syntax.options[obj_syntax.selectedIndex].value;
+		if (kind_syntax == 'html'){
+			code = syntax_html(code);
+		}else if (kind_syntax == 'c'){
+			code = syntax_c(code);
+		}else{
+			console.log('invalid syntax_kind :"' + kind_syntax +'"');
+		}
+		// HTML改行を付与する
+		code = add_linefeed(code);
+		// 行番号を付与する
+		if (obj_is_lnum.checked){
+			// FIXME: 改行種別を判定する
+			code = add_linenumber(code);
+		}
+		// 枠(および標準の装飾)を付与する
+		code = add_border(code);
+
+		// ソースを表示する
+		obj_preview.innerHTML = code;
+		obj_output.value = code;
+	}
+
+
+	// 外枠を追加する
+	function add_border(code){
+		code = '<div style="font-family: monospace; '
+			+ ' background-color: #fcfcfc; '
+			+ ' border: 1px solid #ccbbcc; ">' + "\n"
+				+ code + "\n"
+				+ "</div>\n"
+		return code;
+	}
+
+	// HTML改行(行末<br />)を加える
+	function add_linefeed(code){
+		//code=code.replace(/^(.*)$/mg,"$1<br />");
+		code=code.replace(/\n/g,"<br />\n");
+		return code;
+	}
+
+	// 論理行番号を加える
+	function add_linenumber(code){
+		var new_code = '';
+		var codes = new Array();
+		codes = code.split("\n");
+		var ofs = 2;	// 行番号の文字数
+		if (99 < codes.length){
+			ofs = 4;
+		}
+		for (var i = 0 ; i < codes.length ; i++){
+			var slnum = String(i+1);
+			for (var order = slnum.length; order < 3; order++){
+				console.log('lo:' + order);
+				slnum = '&nbsp;' + slnum;
+			}
+			new_code += slnum + '| ' + codes[i] + "\n";
+		}
+		return new_code;
+	}
+
+	// HTMLタグなどをエスケープする
+	function htmlspecialchars(code) { 
+		code = code.replace(/&/g,"&amp;") ;
+		code = code.replace(/"/g,"&quot;") ;
+		code = code.replace(/'/g,"&#039;") ;
+		code = code.replace(/</g,"&lt;") ;
+		code = code.replace(/>/g,"&gt;") ;
+		// 半角空白を処理する
+		//code = code.replace(/ /g,"&#32;") ;
+		code = code.replace(/\x20/g,"&ensp;") ;
+		// タブを処理する
+		var tab = '<pre style="margin-top:0pt;margin-bottom:0pt;'
+			+ 'display: inline-block; _display: inline;" >&#x0009;</pre>' + "";
+		code = code.replace(/\t/g, tab);
+		return code ;
+	}
+
+	// HTMLシンタックスハイライト(<>をカラー表示)
+	function syntax_html(code, color){
+		code = keyword_html(code);
+		if (typeof color === "undefined"){
+			color = '#008800'; // 緑
+		}
+		replace = '<span style="color:' + color + '">$1</span>';
+		code = code.replace(/(&lt;(.*?)&gt;)/g, replace);
+		return code;
+	}
+
+	// HTMLキーワード(予約語)ハイライト
+	function keyword_html(code){
+		var regex;
+		// <>で囲われた範囲内の予約後をハイライトする
+		// 要素レベル
+		// var keywords = (head|body|h[0-9]|br|div|font
+		//		|form|input|select|option)(.*&gt;)/');
+
+		var keywords = new Array('style', 'width', 'height', 'class', 'id', 
+				'action', 'method', 'type', 'name', 'value');
+		for ( var i = 0; i < keywords.length; i++){
+			// 属性レベル
+			regex = new RegExp('(&lt;.*?\\s)('+ keywords[i] +')(=.*&gt;)', 'g');
+			var color = '#000088';
+			replace = '$1<span style="color:' + color + '">$2</span>$3';
+			//code = code.replace(regex, replace);
+			code = code.replace(regex, replace);
+		}
+		return code;
+	}
+
+	// C系言語をシンタックスハイライトする
+	function syntax_c(code){
+		return code;
+	}
+
